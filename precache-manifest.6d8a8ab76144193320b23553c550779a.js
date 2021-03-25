8581ee151fd626c8d7fa8a6283ea5223344b0491
eireland <eireland@concord.org>
2021-03-25 15:18:26 -0700

    Updates

---
+++ precache-manifest.6d8a8ab76144193320b23553c550779a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62bf0d35c76be86a09e2fe6e9ad5886f",
+    "url": "/codap-data-interactive-react/index.html"
+  },
+  {
+    "revision": "40f989c8b667935ff41d",
+    "url": "/codap-data-interactive-react/static/css/main.aaae2060.chunk.css"
+  },
+  {
+    "revision": "937e2c5b225f2c2fe02c",
+    "url": "/codap-data-interactive-react/static/js/2.aea6ae1c.chunk.js"
+  },
+  {
+    "revision": "d829e4495f929217843b66d098998320",
+    "url": "/codap-data-interactive-react/static/js/2.aea6ae1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40f989c8b667935ff41d",
+    "url": "/codap-data-interactive-react/static/js/main.d3007fbb.chunk.js"
+  },
+  {
+    "revision": "5545221cb5f878e7b280",
+    "url": "/codap-data-interactive-react/static/js/runtime-main.632aa334.js"
+  }
+]);
