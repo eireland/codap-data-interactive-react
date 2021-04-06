4039d868d555bec05bec19bc4d3bf3189e364c3f
eireland <eireland@concord.org>
2021-04-06 10:31:01 -0700

    Added a tooltip, but still has positioning problems

--- src/components/header/navigation.jsx
+++ src/components/header/navigation.jsx
@@ -4,23 +4,25 @@ import "./navigation.css"
 
 export const NavigationTabs = (props) => {
   const categories = getCategories(props.plugins);
-  const categoryTooltip = [
-                            "Plugins developed with other project partners",
-                            "Plugins developed to help developers",
-                            "Plugins developed for Data Games project",
-                            "Plugins developed for Data World project"
-                          ]
+  const categoryTooltip = [{ category: "Partners", tooltip: "Plugins developed with other project partners" },
+                           { category: "Utilities", tooltip: "Plugins developed to help developers" },
+                           { category: "Data Games", tooltip: "Plugins developed for Data Games project" },
+                           { category: "Data Worlds", tooltip: "Plugins developed for Data World project" }
+                          ];
+
   return (
     <>
       <div className="navigationTabs">
-        {categories.map((category, index) =>
-          <div key={`${index}`} className={`tab ${category} ${props.categorySelected === category ? "selected" : ""}`} value={category}
-            onClick={()=> props.handleCategorySelect(category)}>
-            {category}
-            <div className="tabTip">
-              <span className="tooltipText">{categoryTooltip[index]}</span>
+        {categories.map((category, index) => {
+          const tip = (categoryTooltip.filter(c => c.category === category))[0];
+          return (
+            <div key={`${index}`} className={`tab ${category} ${props.categorySelected === category ? "selected" : ""}`} value={category}
+              onClick={() => props.handleCategorySelect(category)}>
+              {category}
+              {tip && <span className="tooltipText">{tip.tooltip}</span>}
             </div>
-          </div>
+          )
+        }
         )}
       </div>
     </>
